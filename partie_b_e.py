c064a9e5a6bfef8310e4fa2017d1c024947bfa3e
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-05-03 15:49:00 +0200

    creer le fichier de la partie b-e

---
+++ partie_b_e.py
@@ -0,0 +1,7 @@
+def F(t, Y, mA, G, xA, yA):
+    # Y est le vecteur d'état [xB, yB, dxB/dt, dyB/dt]
+    xB, yB, dxB, dyB = Y
+    r = ((xB - xA)**2 + (yB - yA)**2)**0.5
+    ddxB = G * mA * (xA - xB) / r**3
+    ddyB = G * mA * (yA - yB) / r**3
+    return [dxB, dyB, ddxB, ddyB]
